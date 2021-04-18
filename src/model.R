cc6c5422afa754f8c1e75a7a37dffa384ce7c8b7
Colin Daniel <colin.daniel@apexrms.com>
2021-04-18 16:09:59 -0400

    Update model.R
    Correct comment typos

--- src/model.R
+++ src/model.R
@@ -1,7 +1,7 @@
 library(rsyncrosim)      # Load SyncroSim R package
 myScenario = scenario()  # Get the SyncroSim scenario that is currently running
 
-# Load scenario's input datasheet from SyncroSim library into R dataframe
+# Load scenario's input datasheet from SyncroSim library into an R dataframe
 myInputDataframe = datasheet(myScenario, name="helloworld_InputDatasheet")
 
 # Extract model inputs from this R dataframe and then do calculations
@@ -9,7 +9,7 @@ x = myInputDataframe$x
 a = myInputDataframe$a
 y = x * a
 
-# Copy output into this R dataframe
+# Copy output into an R dataframe
 myOutputDataframe = data.frame(y=y)
 
 # Save this R dataframe back to the SyncroSim library's output datasheet
